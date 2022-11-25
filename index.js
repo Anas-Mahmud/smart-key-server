df0f920e6c9bba0a3c41bf96e4b265b72384ef6a
Anas <anasmahmud523@gmail.com>
2022-11-25 20:18:57 +0600

    get products

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -9,14 +11,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.8octg6m.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8octg6m.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const allProductsCollection = client.db('smartKey').collection('allProducts');
+
+        app.get('/allProducts', async (req, res) => {
+            const query = {};
+            const products = await allProductsCollection.find(query).toArray();
+            res.send(products);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 
 app.get('/', async (req, res) => {
