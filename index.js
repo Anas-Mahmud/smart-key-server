62d68d23c96a584bf01c7614c01e82d28d98ac0f
Anas <anasmahmud523@gmail.com>
2022-11-26 23:31:35 +0600

    get booking by email

--- index.js
+++ index.js
@@ -45,6 +45,14 @@ async function run() {
             res.send(result);
         })
 
+        // get booking by email
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
         // post booking
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
